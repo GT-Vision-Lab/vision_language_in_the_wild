0ac553901f5e20b03db3d0c2584f8185e040fe6d
Arda Pekis <git@ardapekis.com>
2018-04-19 17:38:13 -0400

    Deactivate CLI due to bug.

--- voice/gen_phrases.py
+++ voice/gen_phrases.py
@@ -10,7 +10,14 @@ import json
 import argparse
 
 input_path = './phrases.json'
+voice = "Matthew"
+api = "AWS"
+sample_rate = 16000
+output_path = None
+audio_format = "wav"
+emphasis = "none"
 
+'''
 parser = argparse.ArgumentParser(description="Use a remote TTS API to generate audio for prompts.")
 parser.add_argument("--input_path", type=str, help="Path to json containing list of phrases.", default='./phrases.json')
 parser.add_argument("--api", type=str, help="Choice of API", choices=["AWS"], default="AWS")
@@ -20,6 +27,11 @@ parser.add_argument("--output_path", type=str, help="Folder to save files to")
 parser.add_argument("--audio_format", type=str, help="File format for audio files", choices=["wav"], default="wav")
 parser.add_argument("--emphasis", type=str, help="Level of emphasis on voice", choices=["none", "some", "lots"], default="none")
 parser.parse_args()
+'''
+
+if not voice:
+    print("Name of speaker ('--voice')")
+    sys.exit(-1)
 
 with open(input_path, 'rb') as f:
     phrases = json.load(f)
